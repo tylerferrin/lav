7d01648f7c9340ff527492882b71ca329d9be783
Tyler Ferrin <tyferrin@gmail.com>
2018-04-01 16:56:03 -0700

    add burger component with basic styling / no onclick functions

--- components/MobileNav.vue
+++ components/MobileNav.vue
@@ -1,12 +1,40 @@
 <template>
-
+  <div class="mobile-nav__container">
+    <nuxt-link to="/" class="mobile-nav__route-to">
+      <h1 class="mobile-nav__title">
+        Like a Villain
+      </h1>
+    </nuxt-link>
+    <Burger />
+  </div>
 </template>
 
 <script>
-export default {
+import Burger from '~/components/Burger.vue'
 
+export default {
+  components: {
+    Burger
+  }
 }
 </script>
 
 <style lang="sass">
+  .mobile-nav
+    &__container
+      position: absolute
+      top: 0
+      left: 0
+      padding: 15px 0 0 15px
+      width: 100%
+    &__route-to
+      text-decoration: none
+    &__title
+      display: inline-block
+      color: black
+      font-family: 'Montserrat'
+      font-weight: 200
+      font-size: 2em
+      text-transform: uppercase
+
 </style>

bb7ece498f2dfeb7ec6d3addbf12a7e31701bac5
Tyler Ferrin <tyferrin@gmail.com>
2018-04-01 23:22:53 -0700

    add vue-scrollto package and beadd scroll effect to burger and menu on:click events

---
+++ components/Burger.vue
@@ -0,0 +1,56 @@
+<template>
+  <button v-on:click="openMenuClick" class="burger-container">
+    <div class="burger-container__bar"
+      v-bind:class="{openTop: isClicked}"></div>
+    <div class="burger-container__bar"
+      v-bind:class="{openMid: isClicked}"></div>
+    <div class="burger-container__bar"
+      v-bind:class="{openBot: isClicked}"></div>
+  </button>
+</template>
+
+<script>
+export default {
+  props: [
+    'isClicked',
+    'openMenuClick'
+  ]
+}
+</script>
+
+<style lang="sass">
+  .burger-container
+    position: absolute
+    top: 0
+    right: 0
+    margin: 22.5px 25px 0 0
+    display: inline-flex
+    flex-direction: column
+    justify-content: space-between
+    align-items: center
+    height: 20px
+    border: none
+    background-color: transparent
+    &:hover
+      cursor: pointer
+      outline: none
+    &:focus
+      outline: none
+
+    &__bar
+      height: 1px
+      width: 25px
+      background-color: black
+      transition: all .25s ease-out
+
+  // burger animations
+  .openTop
+    transform: translate(-75%, 0)
+  .openMid
+    transform: translate(-50%, 0)
+  .openBot
+    transform: translate(-25%, 0)
+
+
+
+</style>
--- components/MobileNav.vue
+++ components/MobileNav.vue
@@ -1,20 +1,57 @@
 <template>
-  <div class="mobile-nav__container">
-    <nuxt-link to="/" class="mobile-nav__route-to">
-      <h1 class="mobile-nav__title">
-        Like a Villain
-      </h1>
-    </nuxt-link>
-    <Burger />
-  </div>
+  <section>
+    <div class="mobile-nav__container">
+      <nuxt-link to="/" class="mobile-nav__route-to">
+        <h1 class="mobile-nav__title">
+          Like a Villain
+        </h1>
+      </nuxt-link>
+      <Burger
+        :isClicked="isClicked"
+        :openMenuClick="openMenuClick" />
+    </div>
+
+    <ul class="route-list">
+      <li v-on:click="scrollDown('.content-grid__outer-container')"><nuxt-link to="/events">Events</nuxt-link></li>
+      <li v-on:click="scrollDown('.content-grid__outer-container')"><nuxt-link to="/music" >Music</nuxt-link></li>
+      <li v-on:click="scrollDown('.content-grid__outer-container')"><nuxt-link to="/video" >Video</nuxt-link></li>
+      <li v-on:click="scrollDown('.content-grid__outer-container')"><nuxt-link to="/works" >Works</nuxt-link></li>
+      <li v-on:click="scrollDown('.content-grid__outer-container')"><nuxt-link to="/biography" >Biography</nuxt-link></li>
+      <li v-on:click="scrollDown('.content-grid__outer-container')"><nuxt-link to="/contact" >Contact</nuxt-link></li>
+    </ul>
+  </section>
 </template>
 
 <script>
 import Burger from '~/components/Burger.vue'
+import scroller from 'vue-scrollto'
 
 export default {
   components: {
     Burger
+  },
+  data () {
+    return {
+      isClicked: false,
+      openMenuClick: () => {
+        this.isClicked = !this.isClicked
+        scroller.scrollTo('.mobile-nav__container', {
+          duration: 700,
+          easing: 'linear',
+          offset: -75
+        })
+      },
+      scrollDown: (element) => {
+        setTimeout(() => {
+          console.log('1 sec')
+          scroller.scrollTo(element, {
+            duration: 700,
+            easing: 'linear',
+            offset: -75
+          })
+        }, 150)
+      }
+    }
   }
 }
 </script>
@@ -22,11 +59,12 @@ export default {
 <style lang="sass">
   .mobile-nav
     &__container
-      position: absolute
+      position: fixed
       top: 0
       left: 0
-      padding: 15px 0 0 15px
+      padding: 15px 0 0 29px
       width: 100%
+      z-index: 2
     &__route-to
       text-decoration: none
     &__title
@@ -36,5 +74,29 @@ export default {
       font-weight: 200
       font-size: 2em
       text-transform: uppercase
+
+  .route-list
+    position: absolute
+    top: 0
+    left: 0
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+    height: 100vh
+    width: 100vw
+    list-style: none
+    padding: 0
+    li
+      padding: 10px 0 10px 0
+      a
+        text-decoration: none
+        color: black
+        font-family: 'Montserrat'
+        font-weight: 200
+        font-size: 1.75em
+        text-transform: uppercase
+
+
 
 </style>

6caedd471b5da924484c439237d541e8b2f54aa1
Tyler Ferrin <tyferrin@gmail.com>
2018-04-09 14:43:42 -0700

    add background image, css-grid and data to contact page -- not responsive yet

--- components/ContactGrid.vue
+++ components/ContactGrid.vue
@@ -1,13 +1,113 @@
 <template>
-  <section>
+  <section class="contact__container">
+    <div class="contact__booking info">
+      <p>Booking</p>
+      <a
+        href='mailto:${contacts.booking}'
+        target="_blank">
+        {{ contacts.booking }}
+      </a>
+    </div>
+    <!-- <div class="contact__clear"></div> -->
+    <div class="contact__general info">
+      <p>General Inquiries</p>
+      <a
+        href='mailto:${contacts.general}'
+        target="_blank">
+        {{ contacts.general }}
+      </a>
+    </div>
+
+    <div class="contact__social info">
+      <ul>
+        <li>
+          <a
+            :href="contacts.instagram"
+            target="_blank">
+            Instagram
+          </a>
+        </li>
+        <li>
+          <a
+            :href="contacts.twitter"
+            target="_blank">
+            Twitter
+          </a>
+        </li>
+        <li>
+          <a
+            :href="contacts.facebook"
+            target="_blank">
+            Facebook
+          </a>
+        </li>
+      </ul>
+    </div>
 
   </section>
 </template>
 
 <script>
-
+export default {
+  data () {
+    return {
+      contacts: this.$store.state.contacts[0]
+    }
+  }
+}
 </script>
 
 <style lang="sass">
+  .contact
+    &__container
+      filter: grayscale(100%)
+      display: grid
+      grid-gap: 1px
+      grid-template-columns: 25% 25% 25% 25%
+      grid-template-rows: 225px 225px
+      background-image: url('~/assets/lav-wide.jpeg')
+      background-size: cover
+      background-position: center
+      background-repeat: no-repeat
+    &__booking
+      grid-column-start: 1
+      grid-column-end: 2
+    &__general
+      grid-column-start: 3
+      grid-column-end: 4
+    &__social
+      grid-row-start: 2
+      grid-column-start: 4
+    &__social.info
+      ul
+        position: relative
+        top: 25%
+        padding-left: 0
+        li
+          list-style: none
+          a
+            display: block
+            position: relative
+            padding: 7.5px 0
+            font-size: 12px
+            text-transform: uppercase
+
+
+  .info
+    position: relative
+    background-color: lighten(grey, 45%)
+    padding: 15px
+    font-family: 'Montserrat'
+    p
+      font-size: 12px
+      text-transform: uppercase
+      position: absolute
+      top: 25%
+    a
+      text-decoration: none
+      font-size: 10px
+      position: absolute
+      top: 40%
+
 
 </style>
